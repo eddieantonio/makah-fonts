9d60397e3e925d81e963efe7f4545c15624f4b39
Eddie Antonio Santos <eddieantonio@hey.com>
2021-03-22 13:47:33 -0600

    feat: can extract font names

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+import Font from "./Font.svelte";
+
 let exampleSentence = "ʔux̌u·ʔaƛa·k,  weʔič̓   šuʔuk̓ʷaƛ̓  ɫax̌"
 </script>
 
@@ -19,10 +21,16 @@ let exampleSentence = "ʔux̌u·ʔaƛa·k,  weʔič̓   šuʔuk̓ʷaƛ̓  ɫax
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td> Andika </td>
-        <td style="font-family: Andika;">{exampleSentence}</td>
-      </tr>
+      <Font fontName="Andika">{exampleSentence}</Font>
+      <Font fontName="Times">{exampleSentence}</Font>
+      <Font fontName="Comic Sans MS">{exampleSentence}</Font>
+      <Font fontName="Tahoma">{exampleSentence}</Font>
+      <Font fontName="Calibri">{exampleSentence}</Font>
+      <Font fontName="Lucida Sans Unicode">{exampleSentence}</Font>
+      <Font fontName="Papyrus">{exampleSentence}</Font>
+      <Font fontName="Trebuchet MS">{exampleSentence}</Font>
+      <Font fontName="Montserrat">{exampleSentence}</Font>
+      <Font fontName="Cambria">{exampleSentence}</Font>
     </tbody>
   <table>
 </main>
---
+++ src/Font.svelte
@@ -0,0 +1,8 @@
+<script>
+export let fontName;
+</script>
+
+<tr style="font-family: '{fontName}'">
+  <td> {fontName} </td>
+  <td><slot></slot></td>
+</tr>

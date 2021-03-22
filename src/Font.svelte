f55863f00c8d6435ccb4b6188b652fd4150d9fdc
Eddie Antonio Santos <eddieantonio@hey.com>
2021-03-22 16:21:52 -0600

    Make Google Fonts work

--- src/Font.svelte
+++ src/Font.svelte
@@ -2,6 +2,12 @@
 export let font;
 </script>
 
+<svelte:head>
+{#if font.isAvailableOnGoogleFonts}
+  <link rel="stylesheet" href={font.googleFontsURL} />
+{/if}
+</svelte:head>
+
 <tr style={font.css}>
   <td>{font.family}</td>
   <td><slot></slot></td>

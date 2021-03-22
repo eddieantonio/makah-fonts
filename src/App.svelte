c400cfb562ffb795601ce1efb3db4fead41f22d8
Eddie Antonio Santos <eddieantonio@hey.com>
2021-03-22 13:38:55 -0600

    make first app

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,28 @@
 <script>
-export let name = "World";
+let exampleSentence = "ʔux̌u·ʔaƛa·k,  weʔič̓   šuʔuk̓ʷaƛ̓  ɫax̌"
 </script>
 
-<h1>hi there, {name}!</h1>
+<header>
+  <h1>Makah Font test</h1>
+</header>
+
+<form>
+  <label> Sample text: <input bind:value={exampleSentence}></label>
+</form>
+
+<main>
+  <table>
+    <thead>
+      <tr>
+        <th> Font </th>
+        <th> Example </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td> Andika </td>
+        <td style="font-family: Andika;">{exampleSentence}</td>
+      </tr>
+    </tbody>
+  <table>
+</main>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import App from "./App.svelte";
 
 new App({
   target: document.body,
-  props: { name: "losers" },
+  props: {},
 });

fa40252c058c1665c17a1d3103f68841f78cd50b
Eddie Antonio Santos <eddieantonio@hey.com>
2021-03-22 18:08:26 -0600

    fix: use correct purge def

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   purge: {
     enabled: production,
     content: [
-      "./src/**/.*.svelte",
+      "./src/**/*.svelte",
     ],
   },
   darkMode: false, // or 'media' or 'class'
